f0597f0b40d8fcc80f301d81a064c57928f6451e
szwedi <s8144@pjwstk.edu.pl>
2013-05-31 15:02:16 +0200

    prymitywne logowanie

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
+
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -19,6 +23,12 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({secret: 'secret key'}));
+app.use(function(req, res, next){
+	res.locals.session = req.session;
+	next();
+});
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,6 +40,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/loginPanel', routes.loginPanel);
 app.post('/login', routes.login);
+app.get('/logout', routes.logout);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index');
+	res.render('index');
 };
 
 exports.loginPanel = function(req, res){
-  res.render('loginPanel');
+	res.render('loginPanel');
 };
 
 exports.login = function(req, res){
-  res.render('login');
+	if(req.body.login === 's0001' && req.body.password === 'pjwstk311'){
+		req.session.userLogin = req.body.login;
+		req.session.login = true;
+		res.render('login');
+	} else {
+		res.render('index');
+	}
+};
+
+exports.logout = function(req, res){
+	req.session.login = false;
+	res.redirect('/');
 };
