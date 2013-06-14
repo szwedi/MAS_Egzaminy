47a7ed137abed0a2b02052d76b220d64d49223c1
szwedi <s8144@pjwstk.edu.pl>
2013-06-14 10:50:54 +0200

    student bez aktywnych testów - poprawki

--- routes/student/activeTest.js
+++ routes/student/activeTest.js
@@ -11,9 +11,11 @@ exports.studentTest = function(req, res){
 	var studentTest = [];
 	var getStudentTests = function(callback){
 		StudentTests.find({login : req.session.userLogin},function(err, result){
-			for(var i=0; i<result[0].test.length; i++){
-				if(result[0].test[i].done == false)
-					callback(result[0].test[i].idTest);
+			if (result.length > 0){
+				for(var i=0; i<result[0].test.length; i++){
+					if(result[0].test[i].done == false)
+						callback(result[0].test[i].idTest);
+				}
 			}
 		});
 	};
@@ -28,17 +30,17 @@ exports.studentTest = function(req, res){
 };
 
 exports.activeTest = function(req,res){
-	var idTestUrl = req.params.id;
 	Test.find({_id: req.params.id}, function(err,data){
 		res.render('student/active/test', {data : data});
 	});
 };
 
 exports.activeTestPost = function(req,res){
+	var idTestUrl = req.params.id;
 	for (var data in req.body) {
 		var idQuestion = data.slice(0,-1);
 		var answer = data.slice(-1);
-		StudentTests.find({login: req.session.userLogin/*, test : {idTest : req.params.id}*/}, function(err, data){
+		StudentTests.find({login: req.session.userLogin}, function(err, data){
 			console.log(data);
 		});
 	}
